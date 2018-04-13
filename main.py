f1c7674bd7b8f865a1e532682ac62accbce28a0f
Dhawani <dhawani.patel197@gmail.com>
2018-04-13 18:31:11 -0500

    fixed database query to form

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:launchcode@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
-
+app.secret_key = 'abcdefghijklmnopqrstuvwxyz'
 db = SQLAlchemy(app)
 
 class Blog(db.Model):
@@ -22,25 +22,28 @@ def newpost():
     if request.method=='POST':
         blog_title = request.form['title']
         blog_body = request.form['body']
-        new_title = Blog(blog_title)
-        new_body = Blog(blog_body)
-        db.session.add(new_title)
-        db.session.add(new_body)
+        new_blog = Blog(blog_title, blog_body)
+        db.session.add(new_blog)
         db.session.commit()
 
-        blogs = Blog.query.all()
+        blogs = Blog.query.filter_by().all()
         #blog_text = Blog.query.all()
         return render_template('index.html', blogs=blogs)
     else:
         return render_template('newpost.html')
 
-#@app.route('/')
+@app.route('/blog', methods=['GET', 'POST'])
+def main_blog():
+    if request.method=='GET':
+        blog = Blog.query.all()
+        return render_template('index.html', blogs=blog)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     #if request.method=='POST':
      #   blog_title = request.form['']
-     return render_template('index.html')
+     blogs = Blog.query.filter_by().all()
+     return render_template('index.html', blogs=blogs)
 
 if __name__=='__main__':
     app.run()        
